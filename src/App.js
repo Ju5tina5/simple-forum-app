1608f1c7525280dfcbc2d1ed1b4029ad05a9601e
Justinas Pekarskis <jpekarskis@yahoo.com>
2022-03-31 09:37:34 +0300

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import IndexPage from "./pages/IndexPage";
+import UserProfilePage from "./pages/UserProfilePage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/login' element={<LoginPage />}/>
+        <Route path='/register' element={<RegisterPage />}/>
+        <Route path='/' element={<IndexPage />}/>
+        <Route path='/profile/:id' element={<UserProfilePage />}/>
+        <Route path='*' element={<ErrorPage />}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/IndexPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IndexPage = () => {
+    return (
+        <div>
+            <h2>Index Page</h2>
+        </div>
+    );
+};
+
+export default IndexPage;
